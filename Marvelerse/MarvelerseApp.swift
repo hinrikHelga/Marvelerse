c4f7917f7188ea0a8504c36c9d9b9aa157acaed7
Hinrik <hinrik.helga@gmail.com>
2023-03-31 13:26:57 +0200

    Initial Commit

---
+++ Marvelerse/MarvelerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelerseApp.swift
+//  Marvelerse
+//
+//  Created by Hinrik Helgason on 31.3.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
